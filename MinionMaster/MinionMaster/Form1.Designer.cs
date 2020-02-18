f91308d7231c1dff39ca1e61432c3ba1d123f904
Esto <MRCalderon3D@hotmail.es>
2020-02-18 16:19:22 +0100

    need addr

---
+++ MinionMaster/MinionMaster/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MinionMaster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkHealth = new System.Windows.Forms.CheckBox();
+            this.lblHealth = new System.Windows.Forms.Label();
+            this.chkLevelUpEachMatch = new System.Windows.Forms.CheckBox();
+            this.lblActualXp = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblTotalXp = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEnemyHealthAddr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chkHealth
+            // 
+            this.chkHealth.AutoSize = true;
+            this.chkHealth.Location = new System.Drawing.Point(12, 104);
+            this.chkHealth.Name = "chkHealth";
+            this.chkHealth.Size = new System.Drawing.Size(57, 17);
+            this.chkHealth.TabIndex = 17;
+            this.chkHealth.Text = "Health";
+            this.chkHealth.UseVisualStyleBackColor = true;
+            // 
+            // lblHealth
+            // 
+            this.lblHealth.AutoSize = true;
+            this.lblHealth.Location = new System.Drawing.Point(103, 146);
+            this.lblHealth.Name = "lblHealth";
+            this.lblHealth.Size = new System.Drawing.Size(35, 13);
+            this.lblHealth.TabIndex = 16;
+            this.lblHealth.Text = "label1";
+            // 
+            // chkLevelUpEachMatch
+            // 
+            this.chkLevelUpEachMatch.AutoSize = true;
+            this.chkLevelUpEachMatch.Location = new System.Drawing.Point(12, 72);
+            this.chkLevelUpEachMatch.Name = "chkLevelUpEachMatch";
+            this.chkLevelUpEachMatch.Size = new System.Drawing.Size(126, 17);
+            this.chkLevelUpEachMatch.TabIndex = 15;
+            this.chkLevelUpEachMatch.Text = "Level up each match";
+            this.chkLevelUpEachMatch.UseVisualStyleBackColor = true;
+            // 
+            // lblActualXp
+            // 
+            this.lblActualXp.AutoSize = true;
+            this.lblActualXp.Location = new System.Drawing.Point(168, 73);
+            this.lblActualXp.Name = "lblActualXp";
+            this.lblActualXp.Size = new System.Drawing.Size(35, 13);
+            this.lblActualXp.TabIndex = 14;
+            this.lblActualXp.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 38);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(100, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 38);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lblTotalXp
+            // 
+            this.lblTotalXp.AutoSize = true;
+            this.lblTotalXp.Location = new System.Drawing.Point(218, 73);
+            this.lblTotalXp.Name = "lblTotalXp";
+            this.lblTotalXp.Size = new System.Drawing.Size(35, 13);
+            this.lblTotalXp.TabIndex = 22;
+            this.lblTotalXp.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "/";
+            // 
+            // txtEnemyHealthAddr
+            // 
+            this.txtEnemyHealthAddr.Location = new System.Drawing.Point(12, 146);
+            this.txtEnemyHealthAddr.Name = "txtEnemyHealthAddr";
+            this.txtEnemyHealthAddr.Size = new System.Drawing.Size(85, 20);
+            this.txtEnemyHealthAddr.TabIndex = 24;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 196);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEnemyHealthAddr);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTotalXp);
+            this.Controls.Add(this.chkHealth);
+            this.Controls.Add(this.lblHealth);
+            this.Controls.Add(this.chkLevelUpEachMatch);
+            this.Controls.Add(this.lblActualXp);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox chkHealth;
+        private System.Windows.Forms.Label lblHealth;
+        private System.Windows.Forms.CheckBox chkLevelUpEachMatch;
+        private System.Windows.Forms.Label lblActualXp;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblTotalXp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEnemyHealthAddr;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MinionMaster/MinionMaster/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinionMaster
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static bool botIsRunning;
+
+
+        public void startNewThread()
+        {
+            // Run the botmethods in a different thread, then the UI won't freeze.
+            // this starts the above method
+            Thread botMethod = new Thread(mainTrainerMethod);
+            botMethod.Start();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            botIsRunning = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            botIsRunning = true;
+            startNewThread();
+        }
+
+        IntPtr baseAddressNvwgf2 = IntPtr.Zero;
+        IntPtr baseAddressMono = IntPtr.Zero;
+        public void mainTrainerMethod() // Focus world of warcraft windows
+        {
+            // nvwgf2umx.dll+19BE230
+            var dummyprocess = Process.GetProcesses();
+            var prc = Process.GetProcessesByName("MinionMasters");
+            if (prc.Length > 0)
+            {
+                Process mainProcess = prc[0];
+                foreach (ProcessModule module in mainProcess.Modules)
+                {
+                    if (module.ModuleName.Contains("nvwgf2umx"))
+                    {
+                        baseAddressNvwgf2 = module.BaseAddress;
+                    }
+                    if (module.ModuleName.Contains("mono"))
+                    {
+                        baseAddressMono = module.BaseAddress;
+                    }
+                }
+
+
+                VAMemory vMemory = new VAMemory("MinionMasters");
+
+                while (botIsRunning)
+                {
+                    IntPtr handle = new IntPtr(Convert.ToInt64(txtEnemyHealthAddr.Text, 16));
+                    int enemyXp = vMemory.ReadInt32(handle);
+                    Invoke(new Action(() =>
+                    {
+                        lblHealth.Text = enemyXp.ToString();
+
+                    }));
+                    if (enemyXp > 50)
+                        vMemory.WriteInt32(handle, enemyXp - 50);
+                    ////nvwgf2umx.dll+19BE230
+                    //int totalXpAddress = vMemory.ReadInt32((IntPtr)baseAddressNvwgf2 + 0x19BE230);
+
+                    ////nvwgf2umx.dll + 19BE240
+                    //int totalXpAddress2 = vMemory.ReadInt32((IntPtr)baseAddressNvwgf2 + 0x19BE240);
+
+                    //long xpP1 = vMemory.ReadLong((IntPtr)baseAddressMono + 0x2691D0);
+                    //long xpP2 = vMemory.ReadLong((IntPtr)xpP1 + 0x0);
+                    //long xpP3 = vMemory.ReadLong((IntPtr)xpP2 + 0x70);
+                    //long xpP4 = vMemory.ReadLong((IntPtr)xpP3 + 0x88);
+                    //long xpP5 = vMemory.ReadLong((IntPtr)xpP4 + 0x160);
+                    //long xpP6 = vMemory.ReadLong((IntPtr)xpP5 + 0xd8);
+                    //long xpP7 = vMemory.ReadLong((IntPtr)xpP6 + 0x0);
+                    //long xpActual = vMemory.ReadInt32((IntPtr)xpP7 + 0x198);
+
+
+                    //Invoke(new Action(() =>
+                    //{
+                    //    lblActualXp.Text = xpActual.ToString();
+                    //    lblTotalXp.Text = totalXpAddress.ToString();
+                    //}));
+
+                    //if (chkLevelUpEachMatch.Checked)
+                    //{
+                    //    vMemory.WriteInt32((IntPtr)xpP7 + 0x198, totalXpAddress - 1);
+                    //}
+
+                    Thread.Sleep(3000);
+                }
+
+
+
+                //while (botIsRunning)
+                //{
+                //    //nvwgf2umx.dll+19BE230
+                //    int totalXpAddress = vMemory.ReadInt32((IntPtr)baseAddressNvwgf2 + 0x19BE230);
+
+                //    //nvwgf2umx.dll + 19BE240
+                //    int totalXpAddress2 = vMemory.ReadInt32((IntPtr)baseAddressNvwgf2 + 0x19BE240);
+
+                //    long xpP1 = vMemory.ReadLong((IntPtr)baseAddressMono + 0x2691D0); 
+                //    long xpP2 = vMemory.ReadLong((IntPtr)xpP1 + 0x0);
+                //    long xpP3 = vMemory.ReadLong((IntPtr)xpP2 + 0x70);
+                //    long xpP4 = vMemory.ReadLong((IntPtr)xpP3 + 0x88);
+                //    long xpP5 = vMemory.ReadLong((IntPtr)xpP4 + 0x160);
+                //    long xpP6 = vMemory.ReadLong((IntPtr)xpP5 + 0xd8);
+                //    long xpP7 = vMemory.ReadLong((IntPtr)xpP6 + 0x0);
+                //    long xpActual = vMemory.ReadInt32((IntPtr)xpP7 + 0x198);
+
+
+                //    Invoke(new Action(() =>
+                //    {
+                //        lblActualXp.Text  = xpActual.ToString();
+                //        lblTotalXp.Text  = totalXpAddress.ToString();
+                //    }));
+
+                //    if(chkLevelUpEachMatch.Checked)
+                //    {
+                //        vMemory.WriteInt32((IntPtr)xpP7 + 0x198, totalXpAddress - 1);
+                //    }
+
+                //    Thread.Sleep(2000);
+                //}
+
+            }
+            else
+                MessageBox.Show("LC2 window not found");
+
+        }
+
+        private float SetHealth(VAMemory vMemory, int healthFinalAddress, float healthOriginal)
+        {
+            float health = vMemory.ReadFloat((IntPtr)healthFinalAddress + 0x18); // 808 // 288
+            Invoke(new Action(() =>
+            {
+                lblHealth.Text = health.ToString();
+            }));
+            if (chkHealth.Checked)
+            {
+                if (health > healthOriginal)
+                {
+                    healthOriginal = health;
+                }
+
+                vMemory.WriteFloat((IntPtr)healthFinalAddress + 0x18, healthOriginal); // 808 // 288
+            }
+            else
+            {
+                healthOriginal = health;
+            }
+
+            return healthOriginal;
+        }
+
+        private int SetAmmo(VAMemory vMemory, int ammoP2, int ammoOriginal)
+        {
+            int ammo = vMemory.ReadInt32((IntPtr)ammoP2 + 0x8); // 808 // 288
+            Invoke(new Action(() =>
+            {
+                lblActualXp.Text = ammo.ToString();
+            }));
+            if (chkLevelUpEachMatch.Checked)
+            {
+                if (ammo > ammoOriginal)
+                {
+                    ammoOriginal = ammo;
+                }
+
+                vMemory.WriteInt32((IntPtr)ammoP2 + 0x8, ammoOriginal); // 808 // 288
+            }
+            else
+            {
+                ammoOriginal = ammo;
+            }
+
+            return ammoOriginal;
+        }
+
+     
+    }
+}
